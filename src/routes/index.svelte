b52cad81aa771b30378236d3fb2e74eee0ce197e
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2022-03-12 23:44:55 +0100

    make it build

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,3 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-</script>
-
 <script lang="ts">
 	import PlayerButton from '$lib/PlayerButton.svelte';
 	import { page } from '$app/stores';
